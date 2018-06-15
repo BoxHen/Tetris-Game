0fa40ea8f7f8c9da22b375b2723ed895b979f631
Henry <Hchen101@binghamton.edu>
2018-06-14 23:24:22 -0400

    give movement to tetris piece

--- JavaScript/tetris.js
+++ JavaScript/tetris.js
@@ -3,15 +3,15 @@
 block = 40; // each block will be 40x40 pixels
 grid_wide = 10;
 grid_height = 20; // this makes the grid 400x800
-
+/*==============================Variables=====================================*/
 // create the game grid
 var canvas = document.getElementById('tetris');
 var tetrisGrid = canvas.getContext('2d');
 
 tetrisGrid.scale(20, 20);
-tetrisGrid.fillStyle = '#000';
-tetrisGrid.fillRect(0,0, canvas.width, canvas.height); // draws a rect with no fill
-
+// tetrisGrid.fillStyle = '#000';
+// tetrisGrid.fillRect(0,0, canvas.width, canvas.height); // draws a rect with no fill
+/*==============================Constants=====================================*/
 const matrix = [
   [0, 0, 0],
   [1, 1, 1],
@@ -22,7 +22,7 @@ const player = {
   pos: {x: 5, y :5},
   matrix: matrix,
 }
-
+/*==============================Functions=====================================*/
 /* draws the game board*/
 function drawMatrix(matrix, offset){ // offset used to move the block around
   matrix.forEach((row, y) => {
@@ -34,14 +34,50 @@ function drawMatrix(matrix, offset){ // offset used to move the block around
     });
   });
 }
-
+/*============================================================================*/
+// draw the tetris pieces
 function drawPieces(){
+  tetrisGrid.fillStyle = '#000';
+  tetrisGrid.fillRect(0,0, canvas.width, canvas.height); // draws a rect with no fill
   drawMatrix(player.matrix, player.pos);
 }
+/*============================================================================*/
+// controls the pieces animations as they move down automatically
+let dropCounter = 0;
+let dropInterval = 1000; // in ms so this is 1 sec
+let lastTime = 0;
 
-function update(){
+function update(time = 0){ // the time we get from requestAnimationFrame but default it to 0 !!!note: time is a defualt parameter
+  const deltaTime = time - lastTime;
+  lastTime = time;
+
+  dropCounter += deltaTime;
+  if(dropCounter >= dropInterval){ // about every second (depending on the dropInterval) the block will fall
+    player.pos.y++;
+    dropCounter = 0; // reset the counter so block does not disappear from the screen
+  }
   drawPieces();
   requestAnimationFrame(update);
 }
-
+/*============================================================================*/
+// controls piece movemnt as user presses the arrowkeys
+document.addEventListener('keydown', event => {
+  //console.log(event); //used to see what the keycode is
+  if(event.keyCode === 37){ // 37 corresponds to arrow key left
+    player.pos.x--;
+  }
+});
 update();
+
+
+
+
+
+/*================================Notes=======================================*/
+/*
+process:
+1. make the game board and test that (canvas)
+2. make a piece and draw it onto the board
+3. test the piece for movement (offset)
+4.
+*/
