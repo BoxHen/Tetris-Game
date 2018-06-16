8bae9c882cd00352cd3a0d1b46793614fca51542
Henry <Hchen101@binghamton.edu>
2018-06-16 12:36:01 -0400

    tetris pieces are bounded left and right to the board

--- JavaScript/tetris.js
+++ JavaScript/tetris.js
@@ -1,8 +1,4 @@
 
-//Constants
-block = 40; // each block will be 40x40 pixels
-grid_wide = 10;
-grid_height = 20; // this makes the grid 400x800
 /*==============================Variables=====================================*/
   // create the game grid
   var canvas = document.getElementById('tetris');
@@ -18,13 +14,14 @@ grid_height = 20; // this makes the grid 400x800
     [0, 1, 0]
   ];
 
-  const arena = createMatrix(12, 20);
+  const arena = createMatrix(10, 20);
     console.log(arena); console.table(arena);
 
   const piece = { // object in JS
     pos: {x: 5, y :5},
     matrix: matrix
   }
+
 /*==============================Functions=====================================*/ // adds the pieces
   /* draws the game board*/
   function drawMatrix(matrix, offset){ // offset used to move the block around
@@ -46,6 +43,17 @@ grid_height = 20; // this makes the grid 400x800
     drawMatrix(arena, {x:0, y:0});
   }
   /*--------------------------------------------------------------------------*/
+  // drops the piece down based on dropInterval
+  function pieceDrop(){
+    piece.pos.y++;
+    if(collision(arena, piece)){
+      piece.pos.y--;
+      merge(arena, piece);
+      piece.pos.y = 0; // resets the piece back at the top
+    }
+    dropCounter = 0;
+  }
+  /*--------------------------------------------------------------------------*/
   // controls the pieces animations as they move down automatically
   let dropCounter = 0;
   let dropInterval = 1000; // in ms so this is 1 sec
@@ -62,17 +70,6 @@ grid_height = 20; // this makes the grid 400x800
     }
     drawPieces();
     requestAnimationFrame(update);
-  }
-  /*--------------------------------------------------------------------------*/
-  // drops the piece down based on dropInterval
-  function pieceDrop(){
-    piece.pos.y++;
-    if(collision(arena, piece)){
-      piece.pos.y--;
-      merge(arena, piece);
-      piece.pos.y = 0; // resets the piece back at the top
-    }
-    dropCounter = 0;
   }
   /*--------------------------------------------------------------------------*/
   function createMatrix(w,h){
@@ -93,12 +90,16 @@ grid_height = 20; // this makes the grid 400x800
     });
   }
   /*--------------------------------------------------------------------------*/
+  // hcekcs for collision on game board
   function collision(arena, piece){
     const [m, o] = [piece.matrix, piece.pos];
     for(let y = 0; y < m.length; ++y){
       for(let x = 0; x < m[y].length; ++x){
         // checks if it is not zero, has a row, and has a column repectively
         if(m[y][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x + o.x]) !== 0){ // checks if the piece is within the arena(game board)
+          // note: if it reaches bottom or side of the board -> undefined
+          // In JS values are coerced so undefined id coerced to false
+          // if the piece reaches bottom or side, arena[] returns false
           return true;
         }
       }
@@ -106,6 +107,12 @@ grid_height = 20; // this makes the grid 400x800
     return false;
   }
   /*--------------------------------------------------------------------------*/
+  function pieceMove(dir){
+    piece.pos.x += dir;
+    if(collision(arena, piece)){
+      piece.pos.x -= dir;
+    }
+  }
   /*--------------------------------------------------------------------------*/
   /*--------------------------------------------------------------------------*/
   /*--------------------------------------------------------------------------*/
@@ -114,13 +121,13 @@ grid_height = 20; // this makes the grid 400x800
   document.addEventListener('keydown', event => {
     //console.log(event); //used to see what the keycode is
     if(event.keyCode === 37){ // corresponds to arrow key LEFT
-      piece.pos.x--;
+      pieceMove(-1) //piece.pos.x--;
     }
       // else if(event.keyCode === 38) {// corresponds to arrow key UP
       // piece.pos.y--;
         // note: No up in tetris but this may be used in future for a unique mode
     else if (event.keyCode === 39) { // corresponds to arrow key RIGHT
-      piece.pos.x++;
+      pieceMove(1) //piece.pos.x++;
     } else if(event.keyCode === 40){ // corresponds to arrow key DOWN
       pieceDrop();
     }
